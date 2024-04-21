b19bad36810b55da7f9914abe40af19a3f5fb1ad
Emiliano Sasal <94084079+emisasal@users.noreply.github.com>
2024-04-20 21:31:38 -0300

    Seed data for authors and genres

---
+++ prisma/seed.ts
@@ -0,0 +1,7 @@
+import { Prisma } from "@prisma/client"
+// import { prisma } from "../api/client"
+
+import { authorSeed } from "./data/authorSeed"
+import { genreSeed } from "./data/genreSeed"
+
+// Execute seeding with: npx prisma db seed
