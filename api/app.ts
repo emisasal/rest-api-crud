0f5f59d9f22da2ea79af6cf5a4b4e76e470f8f6b
Emiliano Sasal <94084079+emisasal@users.noreply.github.com>
2024-04-24 20:56:50 -0300

    Routes folder and index.ts

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,17 @@
 import express from "express"
+import routes from "./routes"
 
 const app = express()
 
 app.use(express.json())
+
+// Registered routes
+app.use("/api", routes)
+
+// Catch unregistered routes
+app.all("*", (req, res) => {
+  res.status(404).json({ error: `Route ${req.originalUrl} not found` })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port: ${process.env.PORT}`)
