e4672921ca053d7aa1d691327dce20f4ee885408
Emiliano Sasal <94084079+emisasal@users.noreply.github.com>
2024-05-24 18:23:39 -0300

    Author validators update

--- api/validators/author.validators.ts
+++ api/validators/author.validators.ts
@@ -1,13 +1,32 @@
-import { body } from "express-validator"
+import { body, param, query } from "express-validator"
+
+export const getAllAuthorsValidator = [
+  query("page").notEmpty().withMessage("Page can't be empty"),
+  query("sort").notEmpty().withMessage("Sort can't be empty").toLowerCase(),
+  query("order").notEmpty().withMessage("Order can't be empty").toLowerCase(),
+  query("filterBy").optional().toLowerCase(),
+]
 
 export const postAuthorValidator = [
-  body("first_name", "Name must be string").isString(),
-  body("last_name", "Last name must be string").isString(),
-  body("bio", "Bio must be string").optional().isString(),
+  body("first_name")
+    .notEmpty()
+    .withMessage("First name can't be empty")
+    .isString()
+    .withMessage("Name must be string"),
+  body("last_name")
+    .notEmpty()
+    .withMessage("Last name can't be empty")
+    .isString()
+    .withMessage("Last name must be string"),
+  body("bio").optional().isString().withMessage("Bio must be string"),
 ]
 
 export const patchAuthorValidator = [
-  body("first_name", "Name must be string").optional().isString(),
-  body("last_name", "Last name must be string").optional().isString(),
-  body("bio", "Bio must be string").optional().isString(),
+  param("id").notEmpty().withMessage("Id param can't be empty"),
+  body("first_name").optional().isString().withMessage("Name must be string"),
+  body("last_name")
+    .optional()
+    .isString()
+    .withMessage("Last name must be string"),
+  body("bio").optional().isString().withMessage("Bio must be string"),
 ]
