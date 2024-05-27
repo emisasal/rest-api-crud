694d6cf325d8ce87551ff4414b565762496260fe
Emiliano Sasal <emiliano.sasal@gmail.com>
2024-05-26 22:19:41 -0300

    Order validators update

--- api/validators/order.validators.ts
+++ api/validators/order.validators.ts
@@ -1,8 +1,32 @@
-import { body } from "express-validator"
+import { body, query } from "express-validator"
+
+export const getAllOrdersValidator = [
+  query("sort").toLowerCase(),
+  query("order").toLowerCase(),
+  query("customer").optional(),
+  query("dateinit").optional(),
+  query("dateend").optional(),
+]
 
 export const postOrderValidator = [
-  body("customer_id", "Customer Id must be a number").isNumeric(),
-  body("books", "Books must be an array").notEmpty().isArray(),
-  body("books.*.book_id", "Book Id must be a number").notEmpty().isInt(),
-  body("books.*.quantity", "Quantity must be a number").notEmpty().isInt(),
+  body("customer_id")
+    .notEmpty()
+    .withMessage("Body 'customer_id' can't be empty")
+    .isNumeric()
+    .withMessage("Body 'customer_id' must be number"),
+  body("books")
+    .notEmpty()
+    .withMessage("Body 'books' can't be empty")
+    .isArray()
+    .withMessage("Body 'books' must be an array"),
+  body("books.*.book_id")
+    .notEmpty()
+    .withMessage("Body 'book_id' can't be empty")
+    .isInt()
+    .withMessage("Body 'book_id' must be number"),
+  body("books.*.quantity")
+    .notEmpty()
+    .withMessage("Body 'quantity' can't be empty")
+    .isInt()
+    .withMessage("Body 'quantity' must be number"),
 ]
