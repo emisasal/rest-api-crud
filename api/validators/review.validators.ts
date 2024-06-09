f7e393f980ca073f971e8e170ca9bc4509b3efd2
Emiliano Sasal <emiliano.sasal@gmail.com>
2024-06-09 12:54:06 -0300

    Review filters and validators update

--- api/validators/review.validators.ts
+++ api/validators/review.validators.ts
@@ -1,10 +1,61 @@
-import { body } from "express-validator"
+import { query, body } from "express-validator"
+
+export const getAllReviewsValidator = [
+  query("page")
+    .notEmpty()
+    .withMessage("Query 'page' can't be empty")
+    .isInt()
+    .withMessage("Query 'page' must be number"),
+  query("sort")
+    .notEmpty()
+    .withMessage("Query 'sort' can't be empty")
+    .isString()
+    .withMessage("Query 'sort' must be string")
+    .toLowerCase(),
+  query("order")
+    .notEmpty()
+    .withMessage("Query 'order' can't be empty")
+    .isString()
+    .withMessage("Query 'order' must be string")
+    .toLowerCase(),
+  query("book_id")
+    .optional()
+    .isNumeric()
+    .withMessage("Query 'book_id' must be number"),
+  query("customer_id")
+    .optional()
+    .isNumeric()
+    .withMessage("Query 'customer_id' must be number"),
+  query("rateMin")
+    .optional()
+    .isInt()
+    .withMessage("Query 'rateMin' must be integer number"),
+  query("rateMax")
+    .optional()
+    .isInt()
+    .withMessage("Query 'rateMax' must be integer number"),
+]
 
 export const postReviewValidator = [
-  body("book_id", "Book Id must be a number").isNumeric(),
-  body("customer_id", "Customer Id must be a number").isNumeric(),
-  body("rating", "Rating must be a number")
+  body("book_id")
+    .notEmpty()
+    .withMessage("Body 'book_id' can't be empty")
     .isNumeric()
-    .isInt({ min: 1, max: 10 }),
-  body("comment", "Comment must be string").optional().isString(),
+    .withMessage("Body 'book_id' must be number"),
+  body("customer_id")
+    .notEmpty()
+    .withMessage("Body 'customer_id' can't be empty")
+    .isNumeric()
+    .withMessage("Body 'customer_id' must be number"),
+  body("rating", "Rating must be a number")
+    .notEmpty()
+    .withMessage("Body 'rating' can't be empty")
+    .isNumeric()
+    .withMessage("Body 'rating' must be number")
+    .isInt({ min: 1, max: 10 })
+    .withMessage("Body 'rating' must be value between 1 and 10"),
+  body("comment")
+    .optional()
+    .isString()
+    .withMessage("Body 'comment' must be string"),
 ]
