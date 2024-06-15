90e4933fc2ba02df8f9f18d459a37f0a28a7ecf9
Emiliano Sasal <emiliano.sasal@gmail.com>
2024-06-15 14:06:44 -0300

    Jsonwebtoken and cookie-parser install

--- api/controllers/customer.controller.ts
+++ api/controllers/customer.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 import { prisma } from "../config/prismaClient"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import errorHandler from "../utils/errorHandler"
 import capitalizeWords from "../utils/capitalizeWords"
 import paginationHandler from "../utils/paginationHandler"
@@ -163,8 +164,8 @@ export const postRegisterCustomer = async (
       return next(errorHandler(409, "Customer already registered"))
     }
 
-    const salt = await bcrypt.genSalt()
-    const hashPassword = await bcrypt.hash(data.password, salt)
+    const saltRounds = Number(process.env.SALT_ROUNDS)
+    const hashPassword = await bcrypt.hash(data.password, saltRounds)
     data.password = hashPassword
 
     const newCustomer = await prisma.customer.create({
@@ -213,6 +214,11 @@ export const postLoginCustomer = async (
     if (!isValidCustomer) {
       return next(errorHandler(401, "Invalid credentials"))
     }
+
+    // Arreglar error secret!
+    const accessToken = jwt.sign({ sub: loginCustomer.customer_id }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: "1d",
+    })
 
     const { password, created_at, updated_at, ...loginCustomerNoPass } =
       loginCustomer
