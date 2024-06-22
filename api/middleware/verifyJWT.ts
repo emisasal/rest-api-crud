b9095c733dcfdf180687bf1f3d684f864908232b
Emiliano Sasal <emiliano.sasal@gmail.com>
2024-06-22 03:27:09 -0300

    Access and refresh token in cookies

--- api/middleware/verifyJWT.ts
+++ api/middleware/verifyJWT.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 import jwt, { Secret } from "jsonwebtoken"
+import { signAccessJWT } from "utils/handleJWT"
 
 export interface Session extends Request {
   session: { user: string | null }
@@ -9,18 +10,44 @@ const verifyJWT = (req: Session, res: Response, next: NextFunction) => {
   const { access_token, refresh_token } = req.signedCookies
   const { JWT_ACCESS_SECRET, JWT_REFRESH_SECRET } = process.env
   //   req.session.user = null
-  console.log("access_token", access_token)
-  console.log("refresh_token", refresh_token)
 
-  //   const data = jwt.verify(refresh_token, JWT_REFRESH_SECRET as Secret)
-  //   console.log("data", data)
+  const refreshToken = refresh_token
+    ? jwt.verify(refresh_token, JWT_REFRESH_SECRET as Secret)
+    : null
+
+  if (!refreshToken) {
+    return res
+      .clearCookie("access_token")
+      .clearCookie("refresh_token")
+      .status(401)
+      .send({
+        success: false,
+        statusCode: 401,
+        message: "Unauthorized",
+      })
+      .end()
+  }
+
+  const accessToken = access_token
+    ? jwt.verify(access_token, JWT_ACCESS_SECRET as Secret)
+    : null
+
+  if (!accessToken) {
+    const newAccessToken = signAccessJWT(Number(refreshToken?.sub))
+    res.cookie("access_token", newAccessToken, {
+      httpOnly: true, // only access through server
+      secure: process.env.NODE_ENV === "production", // https only
+      sameSite: "strict", // same domain access
+      maxAge: 60 * 1000 * 15, // 15min
+      signed: true, // encrypt cookie (not the content)
+    })
+  }
 
   next()
 }
 
 export default verifyJWT
 
-// Recieve access and refresh tokens
-// If no refresh token return unauthorized
-// If refresh token invalid return unauthorized
-// if valid refresh and no access, create and return new access token
+// Recieve access_token and refresh_token jwt in cookies
+// If invalid or no refresh_token, return 401 and clear cookies
+// if invalid or no access_token with valid refresh_token, create and return new access_token and send cookie
