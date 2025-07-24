6b1199ef868241720569d6dee5fcc4250eb7e44f
Emiliano Sasal <emiliano.sasal@gmail.com>
2025-07-24 10:34:04 -0300

    Added Memory Usage console.log

--- api/server.ts
+++ api/server.ts
@@ -3,4 +3,5 @@ import app from "./app"
 app.listen(process.env.PORT, () => {
   console.log(`Enviroment: ${process.env.NODE_ENV}`)
   console.log(`Server running on port: ${process.env.PORT}`)
+  // console.log("Memory usage:", process.memoryUsage()) // Uncomment to log memory usage
 })
